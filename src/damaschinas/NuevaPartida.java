efb6da88a885b70002e7142e16163fcd3a797384
Luis Monterroso <luismonterroso202031794@cunoc.edu.gt>
2021-06-19 23:57:18 -0600

    Vamos haciendo la partida de damas

---
+++ src/damaschinas/NuevaPartida.java
@@ -0,0 +1,43 @@
+package damaschinas;
+
+import java.util.Scanner;
+
+public class NuevaPartida {
+
+    private VectorJugador vectorJugador;
+    private Tablero tablero = new Tablero();
+    private Scanner scanner = new Scanner(System.in);
+    private JuegoPiedraPapelTijera piedraPapel = new JuegoPiedraPapelTijera();
+
+    public NuevaPartida(VectorJugador vectorJugador) {
+        this.vectorJugador = vectorJugador;
+        pedirJugadores();
+    }
+
+    public void pedirJugadores() {
+        if (vectorJugador.getContadorJugadores() >= 2) {
+            int idPrimerJugador;
+            int idSegundoJugador;
+            vectorJugador.mostrarJugadores();
+            System.out.println("Ingrese el id del primer jugador");
+            idPrimerJugador = scanner.nextInt();
+            System.out.println("Ingrese el id del segundo jugador");
+            idSegundoJugador = scanner.nextInt();
+            if (vectorJugador.verificarId(idPrimerJugador) == true && vectorJugador.verificarId(idSegundoJugador) == true) {
+                if (idPrimerJugador != idSegundoJugador) {
+                    decidirQuienEmpieza(idPrimerJugador, idSegundoJugador);
+                } else {
+                    System.out.println("No puedes jugar contra el mismo jugador");
+                }
+            } else {
+                System.out.println("Id inexistente ingresado, verifique e intentelo mas tarde");
+            }
+        } else {
+            System.out.println("Jugadores insuficientes para una partida");
+        }
+    }
+
+    public void decidirQuienEmpieza(int idPrimerJugador, int idSegundoJugador) {
+        int jugarPiedraPapelYTijera = piedraPapel.jugarPiedraPapelYTijera();
+    }
+}
---
+++ src/damaschinas/Tablero.java
@@ -0,0 +1,67 @@
+package damaschinas;
+
+public class Tablero {
+
+    private Casilla[][] tablero = new Casilla[8][8];
+
+    public Tablero() {
+        llenarTableroPrimeraVez();
+    }
+
+    public void llenarTableroPrimeraVez() {
+        for (int x = 0; x < tablero.length; x++) {
+            for (int y = 0; y < tablero[x].length; y++) {
+                if (x >= 0 && x <= 2) {
+                    if ((x + 1) % 2 == 0) {
+                        if ((y + 1) % 2 == 0) {
+                            tablero[x][y] = new Casilla(false, false, 0);
+                        } else {
+                            tablero[x][y] = new Casilla(true, true, 1);
+                        }
+                    } else {
+                        if ((y + 1) % 2 == 0) {
+                            tablero[x][y] = new Casilla(true, true, 1);
+                        } else {
+                            tablero[x][y] = new Casilla(false, false, 0);
+                        }
+                    }
+                } else if (x >= 5 && x <= 7) {
+                    if ((x + 1) % 2 == 0) {
+                        if ((y + 1) % 2 == 0) {
+                            tablero[x][y] = new Casilla(false, false, 0);
+                        } else {
+                            tablero[x][y] = new Casilla(true, true, 2);
+                        }
+                    } else {
+                        if ((y + 1) % 2 == 0) {
+                            tablero[x][y] = new Casilla(true, true, 2);
+                        } else {
+                            tablero[x][y] = new Casilla(false, false, 0);
+                        }
+                    }
+                } else if ((x + 1) % 2 == 0) {
+                    if ((y + 1) % 2 == 0) {
+                        tablero[x][y] = new Casilla(false, false, 0);
+                    } else {
+                        tablero[x][y] = new Casilla(true, false, 0);
+                    }
+                } else {
+                    if ((y + 1) % 2 == 0) {
+                        tablero[x][y] = new Casilla(true, false, 0);
+                    } else {
+                        tablero[x][y] = new Casilla(false, false, 0);
+                    }
+                }
+            }
+        }
+    }
+
+    public void imprimirTablero() {
+        for (int x = 0; x < tablero.length; x++) {
+            for (int y = 0; y < tablero[x].length; y++) {
+                System.out.print(tablero[x][y].getPieza());
+            }
+            System.out.println("");
+        }
+    }
+}
