fe6d6117a16d7f295f1308d870de3c79199b7130
Luis Monterroso <luismonterroso202031794@cunoc.edu.gt>
2021-06-19 23:54:18 -0600

    Agregada la clase casilla, servira ara crear el tablero

---
+++ src/damaschinas/Casilla.java
@@ -0,0 +1,78 @@
+package damaschinas;
+
+public class Casilla {
+
+    private boolean color;
+    private boolean tienePieza;
+    private String pieza;
+    //Colores de letra
+    private String textoBlanco = "\u001B[37m";
+    private String textoNegro = "\u001B[30m";
+    //Colores de fondo
+    private String negroFondo = "\u001B[40m";
+    private String blancoFondo = "\u001B[47m";
+    // reset
+    private String reset = "\u001B[0m";
+
+    public Casilla(boolean color, boolean tienePieza, int pieza) {
+        this.color = color;
+        this.tienePieza = tienePieza;
+        definirColor(pieza);
+
+    }
+
+    public void definirColor(int pieza) {
+        if (tienePieza == true) {
+            if (color == true) {
+                switch (pieza) {
+                    case 1:
+                        this.pieza = negroFondo + "  X  " + reset;
+                        break;
+                    case 2:
+                        this.pieza = negroFondo + "  O  " + reset;
+                        break;
+                }
+            } else {
+                switch (pieza) {
+                    case 1:
+                        this.pieza = blancoFondo + textoNegro + "  X  " + reset;
+                        break;
+                    case 2:
+                        this.pieza = blancoFondo + textoNegro + "  O  " + reset;
+                        break;
+                }
+            }
+        } else {
+            if (color == true) {
+                this.pieza = negroFondo + "     " + reset;
+            } else {
+                this.pieza = blancoFondo + "     " + reset;
+            }
+        }
+    }
+
+    public boolean isColor() {
+        return color;
+    }
+
+    public void setColor(boolean color) {
+        this.color = color;
+    }
+
+    public boolean isTienePieza() {
+        return tienePieza;
+    }
+
+    public void setTienePieza(boolean tienePieza) {
+        this.tienePieza = tienePieza;
+    }
+
+    public String getPieza() {
+        return pieza;
+    }
+
+    public void setPieza(String pieza) {
+        this.pieza = pieza;
+    }
+
+}
