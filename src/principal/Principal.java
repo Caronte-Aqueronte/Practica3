55c365f461ad61974227b6b55499daf8cc0b9e4b
Luis Monterroso <luismonterroso202031794@cunoc.edu.gt>
2021-06-19 23:51:12 -0600

    Agregada la clase principal

---
+++ src/principal/Principal.java
@@ -0,0 +1,47 @@
+package principal;
+
+import damaschinas.NuevaPartida;
+import damaschinas.VectorJugador;
+import java.util.Scanner;
+
+public class Principal {
+
+    public static void main(String[] args) {
+        Principal damas = new Principal();
+    }
+    
+    private Scanner scanner = new Scanner(System.in);
+
+    private VectorJugador vectorJugador = new VectorJugador();
+    private NuevaPartida nuevaPartida;
+
+    public Principal() {
+        menuPrincipal();
+    }
+
+    public void menuPrincipal() {
+        int opcion = 0;
+        while (opcion != 4) {
+            System.out.println("");
+            System.out.println("***\tBIENVENIDO\t***");
+            System.out.println("1) Generar Tablero\n2) Ingresar Nuevo Jugador\n3) Reportes de partidas ganadas y perdias\n4) Salir");
+            opcion = scanner.nextInt();
+            switch (opcion) {
+                case 1:
+                    nuevaPartida = new NuevaPartida(vectorJugador);
+                    break;
+                case 2:
+                    vectorJugador.addJugador();
+                    break;
+                case 3:
+                    vectorJugador.mostrarReportes();
+                    break;
+                case 4:
+                    break;
+                default:
+                    System.out.println("Opcion invalida");
+                    break;
+            }
+        }
+    }
+}
